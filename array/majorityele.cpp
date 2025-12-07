735bd486487f5c1c3d255145783e5b5d14aa640f
Ayushi Singh <ayushisingh.13096@gmail.com>
2025-12-07 23:49:29 +0530

    maj ele

---
+++ array/majorityele.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<cmath>
+#include <vector>
+#include<climits>
+#include <algorithm>
+using namespace std;
+int main(){
+    int arr[]={1,2,3,4,5,6,8,5,3,2,1,2,3,4,5,2};
+    int n=16;
+    int freq=1;
+    int maxfreq=1;
+    int ans=arr[0];
+    for(int i=1;i<n;i++){
+        if(arr[i]==arr[i-1]){
+            freq++;
+        }
+        else 
+        freq=1,ans=arr[i];
+
+        if(freq>maxfreq){
+        ans=arr[i];
+        maxfreq=freq;
+        }
+    }
+    cout<<ans<<" "<<maxfreq<<endl;
+
+    
+}
